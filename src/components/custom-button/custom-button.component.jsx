2664250fb29eade821b311400bff95cf3631ff1a
Xandre Du Plooy <xandre@uproar.gg>
2020-07-21 13:07:40 +1200

    sign in component

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './custom-button.styles.scss'
+
+const customButton = ({ children, ...otherProps }) => {
+	return (
+		<button className="custom-button" {...otherProps}>
+			{
+				children
+			}
+		</button>
+	)
+}
+
+export default customButton
