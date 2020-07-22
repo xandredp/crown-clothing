25f38244b8dced9c4d00ec2b7164dbf84d005ff9
Xandre Du Plooy <xduplooy@gmail.com>
2020-07-23 11:44:47 +1200

    sign in with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import './sign-in.styles.scss';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth ,signInWithGoogle } from '../../firebase/firebase.utils';
 
 export class SignIn extends Component {
 	constructor() {
@@ -15,9 +15,15 @@ export class SignIn extends Component {
 		}
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
-		this.setState({ password: '', email: '' });
+		const { email, password } = this.state;
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ password: '', email: '' });
+		} catch(error) {
+			console.error(error);
+		}
 	}
 
 	handleChange = event => {
