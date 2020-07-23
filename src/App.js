84e6b484326a9cc5936367ca81d192968d0b70c8
Xandre Du Plooy <xduplooy@gmail.com>
2020-07-24 09:44:42 +1200

    redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
 
@@ -27,10 +27,8 @@ class App extends React.Component {
           });
         });
       }
-      else {
-        setCurrentUser(userAuth);
-      }
-    })
+      setCurrentUser(userAuth);
+    });
   }
 
   componentWillUnmount() {
@@ -44,15 +42,23 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/shop" component={ShopPage}></Route>
-          <Route path="/signin" component={SignInAndSignUpPage}></Route>
+          <Route
+            exact
+            path="/signin"
+            render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)}>
+          </Route>
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
